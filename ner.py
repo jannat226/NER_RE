b46ddcaab216087da4068beef7901aee6d7a3559
jannat226 <jannatchehal09@gmail.com>
2025-10-08 22:44:53 -0400

    NER

--- ner.py
+++ ner.py
@@ -1,11 +1,13 @@
 import json 
 import scispacy
+import spacy
 
 with open('newSample_preprocess.json', 'r', encoding = 'utf-8') as f:
     docs = json.load(f)
     
 nlp = spacy.load("en_ner_bc5cdr_md")
 
+ner_results = {}
 for doc in docs:
     spacy_doc = nlp(doc['text'])
     entities = []
@@ -16,6 +18,10 @@ for doc in docs:
             "text_span" : entity.text,
             "label" : entity.label_,
         })
-    doc['entities'] = entities
+    ner_results[doc['doc_id']] = {
+        "text" : doc['text'],
+        "entities" : entities
+    }
     
-
+with open('sample_ner_results.json', 'w', encoding='utf-8') as f:
+    json.dump( ner_results, f, indent=2 )
