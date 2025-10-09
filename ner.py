ef492c6f40b8332a2a49c307f5bb4269424a8ba8
jannat226 <jannatchehal09@gmail.com>
2025-10-09 16:40:06 -0400

    using qwen for ner

--- ner.py
+++ ner.py
@@ -1,27 +1,62 @@
-import json 
-import scispacy
+# import json 
+# import scispacy
+# import spacy
+
+# with open('newSample_preprocess.json', 'r', encoding = 'utf-8') as f:
+#     docs = json.load(f)
+    
+# nlp = spacy.load("en_ner_bc5cdr_md")
+
+# ner_results = {}
+# for doc in docs:
+#     spacy_doc = nlp(doc['text'])
+#     entities = []
+#     for entity in spacy_doc.ents:
+#         entities.append({
+#             "start" : entity.start_char,
+#             "end" : entity.end_char,
+#             "text_span" : entity.text,
+#             "label" : entity.label_,
+#         })
+#     ner_results[doc['doc_id']] = {
+#         "text" : doc['text'],
+#         "entities" : entities
+#     }
+    
+# with open('sample_ner_results.json', 'w', encoding='utf-8') as f:
+#     json.dump( ner_results, f, indent=2 )
+
+import json
 import spacy
 
-with open('newSample_preprocess.json', 'r', encoding = 'utf-8') as f:
+# Load your preprocessed document list
+with open('newSample_preprocess.json', 'r', encoding='utf-8') as f:
     docs = json.load(f)
-    
-nlp = spacy.load("en_ner_bc5cdr_md")
 
+# Load fine-grained biomedical NER model
+nlp = spacy.load("en_ner_bionlp13cg_md")
+
+# Optionally, list the available entity types for confirmation
+print("Entity labels in model:", nlp.get_pipe('ner').labels)
+
+# Extract entities for each doc
 ner_results = {}
 for doc in docs:
     spacy_doc = nlp(doc['text'])
     entities = []
     for entity in spacy_doc.ents:
         entities.append({
-            "start" : entity.start_char,
-            "end" : entity.end_char,
-            "text_span" : entity.text,
-            "label" : entity.label_,
-        })
+            "start": entity.start_char,
+            "end": entity.end_char,
+            "text_span": entity.text,
+            "label": entity.label_,  # Should be fine-grained, e.g. ORGANISM, SIMPLE_CHEMICAL, etc.        
+    })
+        
     ner_results[doc['doc_id']] = {
-        "text" : doc['text'],
-        "entities" : entities
+        "text": doc['text'],
+        "entities": entities
     }
-    
-with open('sample_ner_results.json', 'w', encoding='utf-8') as f:
-    json.dump( ner_results, f, indent=2 )
+
+# Save the results
+with open('sample_ner_results_bionlp.json', 'w', encoding='utf-8') as f:
+    json.dump(ner_results, f, ensure_ascii=False, indent=2)
